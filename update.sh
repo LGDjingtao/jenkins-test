9a0aafeffda990b7faa2c58af53f4d7e60672606
zjt <396933988@qq.com>
2024-05-11 09:40:23 +0800

    4

--- update.sh
+++ update.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-sudo docker build -t jenkins-test:laster .
+sudo docker build -t jenkins-test:latest .
+fi
+sudo docker run -d --name jenkins-test \
+--restart always \
+--net=host \
+-v /data/alarm-center-data/logs:/senergy/app/logs \
+jenkins-test:latest
